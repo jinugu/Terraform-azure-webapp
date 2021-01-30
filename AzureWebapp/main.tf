138ef814261629821a58fae01e02d0872b7bf7d0
applesoftsol <63569208+applesoftsol@users.noreply.github.com>
2021-01-30 11:34:00 +0530

    Update main.tf

--- AzureWebapp/main.tf
+++ AzureWebapp/main.tf
@@ -1,19 +1,22 @@
 # Configure the Azure provider
 provider "azurerm" {
-  version         = "=2.4.0"
-  subscription_id = "2489a3c7-9e54-4074-a38d-eb3af3bdc14f"
-  client_id       = "598fe56e-920e-428d-b16a-4b9386497ce3"
-  client_secret   = "jL6hqfkrx63b5N-2.E0S5h0.FFpu.5Pp4x"
-  tenant_id       = "c0447ee7-7b4a-40e6-85b3-dc6eda2714de"
+  version = "=2.4.0"
+
+  subscription_id = "395b2b6c-8fb6-4cc1-83e4-39455be6b102"
+  client_id       = "72b2db2e-be38-456a-90ba-6f56877fc782"
+  client_secret   = "pq064c.VJ5_X_a~rwTDKs8In42eQhmmw67"
+  tenant_id       = "90c3f360-0a02-49e4-b70c-4ebb69378edf"
+
   features {}
 }
+
 resource "azurerm_resource_group" "webrg2" {
-  name     = "webapprg1"
+  name     = "webapprg00"
   location = "eastus"
 }
 
 resource "azurerm_app_service_plan" "webplan2" {
-  name                = "slotAppServicePlan04"
+  name                = "slotAppServicePlan05"
   location            = azurerm_resource_group.webrg2.location
   resource_group_name = azurerm_resource_group.webrg2.name
   sku {
@@ -31,14 +34,14 @@ resource "random_id" "randomId" {
   byte_length = 8
 }
 resource "azurerm_app_service" "webapp2" {
-  name                = "slotAppService400${random_id.randomId.hex}"
+  name                = "slotAppService500${random_id.randomId.hex}"
   location            = azurerm_resource_group.webrg2.location
   resource_group_name = azurerm_resource_group.webrg2.name
   app_service_plan_id = azurerm_app_service_plan.webplan2.id
 }
 
 resource "azurerm_app_service_slot" "slotDemo2" {
-    name                = "slotAppServiceSlotOne400${random_id.randomId.hex}"
+    name                = "slotAppServiceSlotOne500${random_id.randomId.hex}"
     location            = azurerm_resource_group.webrg2.location
     resource_group_name = azurerm_resource_group.webrg2.name
     app_service_plan_id = azurerm_app_service_plan.webplan2.id
